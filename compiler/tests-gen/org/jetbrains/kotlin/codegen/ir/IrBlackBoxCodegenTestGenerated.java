727dd0f7f80c53c8b4105fcbbce7c20ef9a57f34
pyos <pyos@google.com>
2020-12-04 12:05:27 +0100

    FIR: serialize correct fqnames for local classes

--- compiler/tests-gen/org/jetbrains/kotlin/codegen/ir/IrBlackBoxCodegenTestGenerated.java
+++ compiler/tests-gen/org/jetbrains/kotlin/codegen/ir/IrBlackBoxCodegenTestGenerated.java
@@ -28460,6 +28460,11 @@ public class IrBlackBoxCodegenTestGenerated extends AbstractIrBlackBoxCodegenTes
                 runTest("compiler/testData/codegen/box/reflection/properties/simpleGetProperties.kt");
             }
 
+            @TestMetadata("withLocalType.kt")
+            public void testWithLocalType() throws Exception {
+                runTest("compiler/testData/codegen/box/reflection/properties/withLocalType.kt");
+            }
+
             @TestMetadata("compiler/testData/codegen/box/reflection/properties/accessors")
             @TestDataPath("$PROJECT_ROOT")
             @RunWith(JUnit3RunnerWithInners.class)
