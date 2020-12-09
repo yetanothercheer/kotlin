6afc1d700bd48b1abd5fd728d46e77c865c9f2ee
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-12-09 15:06:03 +0300

    [Build] Split `:tests-mutes` package to common and TC integration parts
    This is needed because of following problem:
    - :tests-mutes has `implementation` dependency on khttp library
    - khttp has dependency on spek-junit-platform-engine library
    - :tests-common had `testCompile` dependency on :tests-mutes which
        added spek library as as a dependency to all modules which depend
        on :tests-common, including :tests-common-new
    
    Then, if project is configured with JPS then if user tries to run all
      tests in directory in module which uses JUnit 5 (like :tests-common-new)
      then spek library will be added to classpath and junit runner takes
      some platform extension from it which causes NoSuchMethodException
      because spek library was compiled against outdated JUnit 5 version
      and current version doesn't have some API.
    
    So splitting :tests-mutes for two parts fixes this issue, because common
      part (:compiler:tests-mutes) no longer depends on khttp, so spek
      library doesn't spreads to all modules

--- compiler/tests-mutes/build.gradle.kts
+++ compiler/tests-mutes/build.gradle.kts
@@ -6,23 +6,9 @@ plugins {
 
 dependencies {
     api(kotlinStdlib())
-    implementation("khttp:khttp:1.0.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.0")
 }
 
 sourceSets {
-    "main" {
-        projectDefault()
-    }
+    "main" { projectDefault() }
     "test" {}
-}
-
-val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by tasks
-compileKotlin.kotlinOptions.freeCompilerArgs += "-Xskip-runtime-version-check"
-
-val mutesPackageName = "org.jetbrains.kotlin.test.mutes"
-
-application {
-    mainClassName = "$mutesPackageName.MutedTestsSyncKt"
-    applicationDefaultJvmArgs = rootProject.properties.filterKeys { it.startsWith(mutesPackageName) }.map { (k, v) -> "-D$k=$v" }
 }
