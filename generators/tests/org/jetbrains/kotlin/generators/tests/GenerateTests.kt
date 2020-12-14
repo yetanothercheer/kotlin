5b4d9cb4d1d4e43679de4c4956b9f903c4329843
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-12-14 12:27:46 +0300

    [TEST] Drop mechanism for muting tests with .mute files
    This mechanism is deprecated and replaced with muting in database (.csv files)

--- generators/tests/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
+++ generators/tests/org/jetbrains/kotlin/generators/tests/GenerateTests.kt
@@ -26,7 +26,6 @@ import org.jetbrains.kotlin.formatter.AbstractFormatterTest
 import org.jetbrains.kotlin.formatter.AbstractTypingIndentationTestBase
 import org.jetbrains.kotlin.generators.TestGroup
 import org.jetbrains.kotlin.generators.generateTestGroupSuite
-import org.jetbrains.kotlin.generators.model.muteExtraSuffix
 import org.jetbrains.kotlin.generators.util.TestGeneratorUtil.KT_OR_KTS
 import org.jetbrains.kotlin.generators.util.TestGeneratorUtil.KT_OR_KTS_WITHOUT_DOTS_IN_NAME
 import org.jetbrains.kotlin.generators.util.TestGeneratorUtil.KT_WITHOUT_DOTS_IN_NAME
@@ -382,15 +381,15 @@ fun main(args: Array<String>) {
                 model("navigation/gotoSymbol", testMethod = "doSymbolTest")
             }
 
-            testClass<AbstractNavigateToLibrarySourceTest>(annotations = listOf(muteExtraSuffix(".libsrc"))) {
+            testClass<AbstractNavigateToLibrarySourceTest>() {
                 model("decompiler/navigation/usercode")
             }
 
-            testClass<AbstractNavigateJavaToLibrarySourceTest>(annotations = listOf(muteExtraSuffix(".libsrc"))) {
+            testClass<AbstractNavigateJavaToLibrarySourceTest>() {
                 model("decompiler/navigation/userJavaCode", pattern = "^(.+)\\.java$")
             }
 
-            testClass<AbstractNavigateToLibrarySourceTestWithJS>(annotations = listOf(muteExtraSuffix(".libsrcjs"))) {
+            testClass<AbstractNavigateToLibrarySourceTestWithJS>() {
                 model("decompiler/navigation/usercode", testClassName = "UsercodeWithJSModule")
             }
 
@@ -1510,7 +1509,7 @@ fun main(args: Array<String>) {
                 model("incremental/js", extension = null, excludeParentDirs = true)
             }
 
-            testClass<AbstractIncrementalJsKlibCompilerRunnerTest>(annotations = listOf(muteExtraSuffix(".jsklib"))) {
+            testClass<AbstractIncrementalJsKlibCompilerRunnerTest>() {
                 model("incremental/pureKotlin", extension = null, recursive = false)
                 model("incremental/classHierarchyAffected", extension = null, recursive = false)
                 model("incremental/js", extension = null, excludeParentDirs = true)
